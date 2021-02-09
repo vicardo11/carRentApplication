6274f551ee975d6e52c5bf92cfbcb38724f881a1
Patryk Sosinski <sosiniak95@gmail.com>
2021-02-09 09:41:43 +0100

    Initial commit

---
+++ src/test/java/com/carental/carrentapplication/CarRentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carental.carrentapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
