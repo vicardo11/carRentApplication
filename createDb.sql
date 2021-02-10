c64d68f8ae92b7a6f47848345e18591571389746
Patryk Sosiński <62787958+vicardo11@users.noreply.github.com>
2021-02-10 10:48:13 +0100

    Update createDb.sql

--- createDb.sql
+++ createDb.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS users(
-	userId INT AUTO_INCREMENT PRIMARY KEY,
+    userId INT AUTO_INCREMENT PRIMARY KEY,
     firstName VARCHAR(255) NOT NULL,
     lastName VARCHAR(255) NOT NULL,
     age INT NOT NULL,
@@ -8,18 +8,18 @@ CREATE TABLE IF NOT EXISTS users(
 );
 
 CREATE TABLE IF NOT EXISTS cars(
-	carId INT AUTO_INCREMENT PRIMARY KEY,
+    carId INT AUTO_INCREMENT PRIMARY KEY,
     manufacturer VARCHAR(255) NOT NULL,
     model VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS carDetails(
-	carId INT NOT NULL,
+    carId INT NOT NULL,
     engineCapacity DOUBLE NOT NULL,
     productionYear INT NOT NULL,
     horsePower INT NOT NULL,
     gearboxType ENUM('manual', 'automatic') NOT NULL,
-	numberOfDoor SMALLINT NOT NULL,
+    numberOfDoor SMALLINT NOT NULL,
     axleDrive ENUM('front', 'rear', '4x4') NOT NULL,
     avgFuelUsage DOUBLE NOT NULL,
     trunkCapacity DOUBLE NOT NULL,
@@ -27,17 +27,11 @@ CREATE TABLE IF NOT EXISTS carDetails(
 );
 
 CREATE TABLE IF NOT EXISTS reservations(
-	reservationId INT AUTO_INCREMENT PRIMARY KEY,
+    reservationId INT AUTO_INCREMENT PRIMARY KEY,
     startingDateOfReservation DATETIME NOT NULL,
     endingDateOfReservation DATETIME NOT NULL,
-	carId INT NOT NULL,
-   FOREIGN KEY (carId) REFERENCES cars(carId)
-);
-
-CREATE TABLE IF NOT EXISTS users_reservations(
-	userId INT NOT NULL,
-    reservationId INT NOT NULL,
-    FOREIGN KEY(userId) REFERENCES users(userId),
-    FOREIGN KEY(reservationId) REFERENCES reservations(reservationId),
-    PRIMARY KEY(userId, reservationId)
+    carId INT NOT NULL,
+    userId INT NOT NULL,
+   FOREIGN KEY (carId) REFERENCES cars(carId),
+   FOREIGN KEY(userId) REFERENCES users(userId)
 );
